55f4cbb89922e550fd898a081eee097d742c8977
vczh_cp <SND\vczh_cp>
2010-03-31 10:09:57 +0000

    Add several operations

---
+++ ReactiveSharp/ReactiveObjects/ReactiveCount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReactiveSharp.ReactiveObjects
+{
+    class ReactiveCount<T> : ReactiveValueBase<int>
+    {
+        private int counter = 0;
+
+        public ReactiveCount(IReactiveEnumerable<T> source)
+        {
+            source.ValueAdded += new ValueAddedDelegate<T>(source_ValueAdded);
+        }
+
+        private void source_ValueAdded(object sender, ReactiveEnumerableValueAddedEventArgs<T> e)
+        {
+            counter++;
+            ChangeValue(counter);
+        }
+    }
+}
