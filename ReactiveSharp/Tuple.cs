e1abd57f53b6f37507a825e5dcfeaa881bc4fec4
Zihan Chen <vczh@163.com>
2010-03-31 08:32:26 +0000

    First check in

---
+++ ReactiveSharp/Tuple.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReactiveSharp
+{
+    public class Tuple<T1>
+    {
+        public T1 V1 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1)
+        {
+            this.V1 = v1;
+        }
+    }
+
+    public class Tuple<T1, T2>
+    {
+        public T1 V1 { get; set; }
+        public T2 V2 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1, T2 v2)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+        }
+    }
+
+    public class Tuple<T1, T2, T3>
+    {
+        public T1 V1 { get; set; }
+        public T2 V2 { get; set; }
+        public T3 V3 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1, T2 v2, T3 v3)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+            this.V3 = v3;
+        }
+    }
+
+    public class Tuple<T1, T2, T3, T4>
+    {
+        public T1 V1 { get; set; }
+        public T2 V2 { get; set; }
+        public T3 V3 { get; set; }
+        public T4 V4 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1, T2 v2, T3 v3, T4 v4)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+            this.V3 = v3;
+            this.V4 = v4;
+        }
+    }
+
+    public class Tuple<T1, T2, T3, T4, T5>
+    {
+        public T1 V1 { get; set; }
+        public T2 V2 { get; set; }
+        public T3 V3 { get; set; }
+        public T4 V4 { get; set; }
+        public T5 V5 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+            this.V3 = v3;
+            this.V4 = v4;
+            this.V5 = v5;
+        }
+    }
+
+    public class Tuple<T1, T2, T3, T4, T5, T6>
+    {
+        public T1 V1 { get; set; }
+        public T2 V2 { get; set; }
+        public T3 V3 { get; set; }
+        public T4 V4 { get; set; }
+        public T5 V5 { get; set; }
+        public T6 V6 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+            this.V3 = v3;
+            this.V4 = v4;
+            this.V5 = v5;
+            this.V6 = v6;
+        }
+    }
+
+    public class Tuple<T1, T2, T3, T4, T5, T6, T7>
+    {
+        public T1 V1 { get; set; }
+        public T2 V2 { get; set; }
+        public T3 V3 { get; set; }
+        public T4 V4 { get; set; }
+        public T5 V5 { get; set; }
+        public T6 V6 { get; set; }
+        public T7 V7 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+            this.V3 = v3;
+            this.V4 = v4;
+            this.V5 = v5;
+            this.V6 = v6;
+            this.V7 = v7;
+        }
+    }
+
+    public class Tuple<T1, T2, T3, T4, T5, T6, T7, T8>
+    {
+        public T1 V1 { get; set; }
+        public T2 V2 { get; set; }
+        public T3 V3 { get; set; }
+        public T4 V4 { get; set; }
+        public T5 V5 { get; set; }
+        public T6 V6 { get; set; }
+        public T7 V7 { get; set; }
+        public T8 V8 { get; set; }
+
+        public Tuple()
+        {
+        }
+
+        public Tuple(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+            this.V3 = v3;
+            this.V4 = v4;
+            this.V5 = v5;
+            this.V6 = v6;
+            this.V7 = v7;
+            this.V8 = v8;
+        }
+    }
+}
