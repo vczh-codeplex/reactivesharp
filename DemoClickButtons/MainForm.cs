e8c0f89820ca5fc5b4904ac0c7b8aef15ace8885
Zihan Chen <vczh@163.com>
2010-04-04 15:33:44 +0000

    Listen to many buttons

---
+++ DemoClickButtons/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ReactiveSharp;
+
+namespace DemoClickButtons
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Controls
+                .Cast<Control>()
+                .Where(c => c.GetType() == typeof(Button))
+                .AttachEvent<EventArgs>("Click")
+                .Count()
+                .Listen(i => this.Text = "You have clicked " + i.ToString() + " times.");
+        }
+    }
+}
